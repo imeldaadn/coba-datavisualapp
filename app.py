882ac20fd17d2acda3c6f2df1c3a0f42a4b09a93
imeldaaudina <126536048+imeldaadn@users.noreply.github.com>
2024-05-07 12:52:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.graph_objects as px
- 
+
 st.title("Halo, This is Imelda Audina!")
 st.markdown("Dataset Data Visual ini diambil dari data Tips.csv")
 
@@ -11,53 +11,50 @@ st.markdown("Dataset Data Visual ini diambil dari data Tips.csv")
 st.subheader("Diagram Batang")
 # reading the database
 data = pd.read_csv("tips.csv")
- 
+
 # Bar chart with day against tip
 plt.bar(data['day'], data['tip'])
- 
 plt.title("Bar Chart")
- 
-# Setting the X and Y labels
 plt.xlabel('Day')
 plt.ylabel('Tip')
 
-# Adding the legends
-plt.show()
+# Now use st.pyplot to render the plot
+st.pyplot()
 
 ############################################################
 st.subheader("Histogram")
 # reading the database
 data = pd.read_csv("tips.csv")
- 
+
 sns.histplot(x='total_bill', data=data, kde=True, hue='sex')
 
-# Adding the legends
-plt.show()
+# Use st.pyplot to render the histogram
+st.pyplot()
 
 ############################################################
 st.subheader("Sliders and Selectors")
 # reading the database
 data = pd.read_csv("tips.csv")
- 
+
 plot = px.Figure(data=[px.Scatter(
-    y=data['tip'],
-    mode='lines',)
+  y=data['tip'],
+  mode='lines',)
 ])
- 
+
 plot.update_layout(
-    xaxis=dict(
-        rangeselector=dict(
-            buttons=list([
-                dict(count=1,
-                    step="day",
-                    stepmode="backward"),
-            ])
-        ),
-        rangeslider=dict(
-            visible=True
-        ),
-    )
+  xaxis=dict(
+    rangeselector=dict(
+      buttons=list([
+        dict(count=1,
+            step="day",
+            stepmode="backward"),
+      ])
+    ),
+    rangeslider=dict(
+      visible=True
+    ),
+  )
 )
 
-# Adding the legends
-plt.show()
+# Use st.plotly_chart to render the plotly figure
+st.plotly_chart(plot)
