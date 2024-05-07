552a38cbbd0b9238792cb9226c2160b465e13e26
imeldaaudina <126536048+imeldaadn@users.noreply.github.com>
2024-05-07 12:44:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.graph_objects as px
- 
+
 st.title("Halo, This is Imelda Audina!")
 st.markdown("Dataset Data Visual ini diambil dari data Tips.csv")
 
@@ -11,33 +11,38 @@ st.markdown("Dataset Data Visual ini diambil dari data Tips.csv")
 st.subheader("Diagram Batang")
 # reading the database
 data = pd.read_csv("tips.csv")
- 
+
 # Bar chart with day against tip
 plt.bar(data['day'], data['tip'])
- 
 plt.title("Bar Chart")
- 
-# Setting the X and Y labels
 plt.xlabel('Day')
 plt.ylabel('Tip')
+
+# Display the plot using st.pyplot()
+st.pyplot()
 
 ############################################################
 st.subheader("Histogram")
 # reading the database
 data = pd.read_csv("tips.csv")
- 
+
+# Create the histogram using seaborn
 sns.histplot(x='total_bill', data=data, kde=True, hue='sex')
+
+# Display the plot using st.pyplot()
+st.pyplot()
 
 ############################################################
 st.subheader("Sliders and Selectors")
 # reading the database
 data = pd.read_csv("tips.csv")
- 
+
+# Create the plotly figure
 plot = px.Figure(data=[px.Scatter(
     y=data['tip'],
     mode='lines',)
 ])
- 
+
 plot.update_layout(
     xaxis=dict(
         rangeselector=dict(
@@ -52,3 +57,6 @@ plot.update_layout(
         ),
     )
 )
+
+# Display the plot using st.plotly_chart()
+st.plotly_chart(plot)
